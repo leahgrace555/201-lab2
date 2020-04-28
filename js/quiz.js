e5bd0d73bad6628accdd8e62733c12a37c8a6d13
Leah Russo <63461171+leahgrace555@users.noreply.github.com>
2020-04-28 19:16:49 -0400

    add quiz page

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,17 @@
+'use strict';
+
 alert('Please answer the following questions with either "yes" or "no"')
 
-var favColor = prompt('Is Leah\'s favorite color purple?'){
-  if favColor = 
-}
+//To try to get the user to answer with acceptable values
+
+var favColor = prompt('Is Leah\'s favorite color purple?');
+
+console.log(favColor);
+
+
+if(favColor.toLowerCase()=== 'yes'){
+  alert('Correct!');
+} else if(favColor.toLowerCase() === 'no'){
+  alert('That is incorrect. Purple is a great color.');
+} else('Following instructions isn\'t really your thing, huh?');
+
