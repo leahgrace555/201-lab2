2384b0389e709135f967d19df5c28b3c07c64107
Leah Russo <63461171+leahgrace555@users.noreply.github.com>
2020-04-28 19:04:06 -0400

    added content to home page

---
+++ js/app.js
@@ -0,0 +1,15 @@
+
+function welcome() {
+  var yourName = prompt('Please enter your name, Please type your name here...');
+
+  var response = yourName;
+  return response;
+
+}
+
+
+
+document.write( welcome() );
+
+
+
---
+++ js/quiz.js
@@ -0,0 +1,5 @@
+alert('Please answer the following questions with either "yes" or "no"')
+
+var favColor = prompt('Is Leah\'s favorite color purple?'){
+  if favColor = 
+}
