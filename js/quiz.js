2ee2a4bc673184cbe58762694bb2c885c0a3f425
Leah Russo <63461171+leahgrace555@users.noreply.github.com>
2020-04-29 17:16:51 -0400

    added prompt that totals the number of correct answers

--- js/quiz.js
+++ js/quiz.js
@@ -4,6 +4,9 @@ alert('Please answer the following questions with either "yes" or "no"')
 
 //To try to get the user to answer with acceptable values
 
+var numCorrect = 0;
+
+console.log(numCorrect);
 
 var favColor = prompt('Is Leah\'s favorite color purple?'); // asking the first qeustion
 
@@ -13,6 +16,8 @@ var favColor = prompt('Is Leah\'s favorite color purple?'); // asking the first 
 
 if(favColor.toLowerCase()=== 'yes'){
   alert('Correct! Purple is an awesome color'); //letting the user know they are correct if they answer yes
+  numCorrect = numCorrect + 1;
+  console.log(numCorrect);
 } else if(favColor.toLowerCase() === 'no'){
   alert('That is incorrect. Purple is a great color.'); //letting the user know they are incorrect if they answer no
 } else{
@@ -29,6 +34,7 @@ if(pets.toLowerCase() ==='yes'){
   alert('Incorrect. Rabbits are the superior form of furry companion'); //letting the user know they are wrong
 } else if(pets.toLowerCase() === 'no'){
   alert('That\'s Correct! Everyone knows rabbits are the superior furry companions.'); //alerting the user they are correct if they answer no
+  numCorrect = numCorrect + 1;
 }
 
 /* I was trying to see if i could get it to say a different answer ONLy if you didn't put yes or no for BOTH the first and second questions but I needed to stop wasting time and actually complete the assignment. 
@@ -54,6 +60,7 @@ var coffee = prompt('Can Leah function in the morning without her coffee?');
 
 if(coffee.toLowerCase() ==='yes'){
   alert('Correct. Coffee makes leah jittery and unable to focus on work.');
+  numCorrect = numCorrect +1;
 } else if(coffee.toLowerCase() === 'no'){
   alert('Incorrect. Caffeine addiction is no joke. Leah prefers a cup of herbal tea in the morning.');
 } else{
@@ -70,6 +77,7 @@ if(icecream.toLowerCase() === 'yes'){
   alert('Incorrect! that is so boring! Ironically, her favorite flavor is coffee.');
 } else if(icecream.toLowerCase() === 'no'){
   alert('Correct! Ironically, her favorite flavor of ice cream is coffee.');
+  numCorrect = numCorrect + 1;
 } else{
   alert('Following instructions isn\'t really your thing, huh?');
 }
@@ -80,6 +88,7 @@ var knitting = prompt('Does Leah know how to knit or crochet?');
 
 if(knitting.toLowerCase() === 'yes'){
   alert('That\'s right! Leah loves doing both in her free time and sells her crafts on Etsy. ');
+  numCorrect = numCorrect + 1;
 } else if(knitting.toLowerCase() ==='no'){
   alert('Wow. That is incorrect, and Leah would be so upset if she heard you say that. Yarn is life.');
 } else{
@@ -93,7 +102,8 @@ for(var i = 0; i < 4; i++){
   var guessNum = prompt('Guess a number between 0 and 1000!!');
   guessNum = parseInt(guessNum);
   if(guessNum === 42 ){
- var correct = alert('Correct! The answer is always 42.')
+ var correct = alert('Correct! The answer is always 42.');
+ numCorrect = numCorrect + 1;
   break;
 } else if(guessNum < 42){
   alert('Whoops! Too low');
@@ -106,6 +116,12 @@ for(var i = 0; i < 4; i++){
 
 
 }
+
+alert('The correct answer is always 42');
+
+alert('congrats! you got ' + numCorrect + ' out of 7 correct.')
+
+
 
 
 
